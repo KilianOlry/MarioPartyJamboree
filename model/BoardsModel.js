fed13babaa909eb5aa57edd34038301312448b75
KilianOlry <kilian.olry@gmail.com>
2025-02-08 22:04:49 +0100

    feat: Add audio files for the boards

--- model/BoardsModel.js
+++ model/BoardsModel.js
@@ -5,6 +5,7 @@ export const boards = [
     description: "Un bois mystérieux et vibrant, peuplé par les redoutables Wiggler géants.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-1.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-1.png'),
+    audio: require('../assets/sounds/Bois_Rieur_de_Mega_Wiggler.mp3'),
   },
   {
     id: '2',
@@ -12,6 +13,7 @@ export const boards = [
     description: "Un circuit rapide et sinueux, idéal pour les courses effrénées.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-2.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-2.png'),
+    audio: require('../assets/sounds/Circuit_debride.mp3'),
   },
   {
     id: '3',
@@ -19,6 +21,7 @@ export const boards = [
     description: "Une île pleine de Goombas et d'obstacles, un vrai défi pour les aventuriers.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-3.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-3.png'),
+    audio: require('../assets/sounds/Ile_Goomba.mp3'),
   },
   {
     id: '4',
@@ -26,6 +29,7 @@ export const boards = [
     description: "La base secrète de Bowser, avec des pièges sournois et des ennemis redoutables.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-7.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-7.png'),
+    audio: require('../assets/sounds/Base_Secrete_de_Bowser.mp3'),
   },
   {
     id: '5',
@@ -33,6 +37,7 @@ export const boards = [
     description: "Un lieu magique aux couleurs vives, offrant un parcours fascinant et surprenant.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-4.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-4.png'),
+    audio: require('../assets/sounds/Galeries_Multicolores.mp3'),
   },
   {
     id: '6',
@@ -40,6 +45,7 @@ export const boards = [
     description: "Un environnement sauvage inspiré des classiques westerns, plein de duels et de défis.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-5.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-5.png'),
+    audio: require('../assets/sounds/Pays_Western.mp3'),
   },
   {
     id: '7',
@@ -47,5 +53,6 @@ export const boards = [
     description: "Un château magique rempli de mystères et de couleurs éclatantes.",
     boardView: require('../assets/images/board-view/super-mario-party-jamboree-website-board-6.jpg'),
     boardIcon: require('../assets/images/board-icons/super-mario-party-jamboree-board-icon-6.png'),
+    audio: require('../assets/sounds/Chateau_Arc-en-Ciel_de_Mario.mp3'),
   }
 ];
